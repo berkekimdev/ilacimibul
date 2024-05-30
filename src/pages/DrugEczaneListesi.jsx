abc2c99086eb02d6ceba84daf876fd7669b5b9ba
berkekimdev <berkekim1@gmail.com>
2024-05-30 08:07:34 +0300

    30.05.2024 4

--- src/pages/DrugEczaneListesi.jsx
+++ src/pages/DrugEczaneListesi.jsx
@@ -17,12 +17,13 @@ const DrugEczaneListesi = () => {
         // Her bir stok için eczane bilgilerini çek
         const enrichedStocks = await Promise.all(filteredStocks.map(async stock => {
           const userResponse = await axios.get(`http://localhost:8080/api/users/${stock.userId}`);
-          console.log('User Data:', userResponse.data); // API'den dönen kullanıcı verilerini kontrol et
           return {
             ...stock,
             eczaneAdi: userResponse.data.eczaneAdi,
             eczaneAdresi: userResponse.data.address,
-            eczaneTelefonu: userResponse.data.phoneNumber
+            eczaneTelefonu: userResponse.data.phoneNumber,
+            eczaneLatitude: userResponse.data.latitude, // Enlem bilgisi
+            eczaneLongitude: userResponse.data.longitude // Boylam bilgisi
           };
         }));
 
@@ -37,24 +38,35 @@ const DrugEczaneListesi = () => {
   }, [drugId]);
 
   return (
-    <div className="eczane-stok-listesi">
+    <div className="eczane-stok-listesi-container">
       <h2>Eczanelerdeki İlaç Stokları</h2>
-      <table>
+      <table className="eczane-stok-listesi-tablosu">
         <thead>
           <tr>
             <th>Eczane Adı</th>
             <th>Eczane Adresi</th>
             <th>Eczane Telefonu</th>
             <th>İlaç Stoğu</th>
+            <th>Konum</th>
           </tr>
         </thead>
         <tbody>
           {eczaneStoklari.map((stok, index) => (
             <tr key={index}>
               <td>{stok.eczaneAdi}</td>
-              <td>{stok.eczaneAdresi}</td>  
-              <td>{stok.eczaneTelefonu}</td> 
+              <td>{stok.eczaneAdresi}</td>
+              <td>{stok.eczaneTelefonu}</td>
               <td>{stok.quantity}</td>
+              <td>
+                <a
+                  href={`https://www.google.com/maps/search/?api=1&query=${stok.eczaneLatitude},${stok.eczaneLongitude}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="konum-goster-link"
+                >
+                  Konumu Göster
+                </a>
+              </td>
             </tr>
           ))}
         </tbody>
